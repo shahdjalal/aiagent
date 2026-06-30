eea15027ec250642e3854e299a162fb36df29fc9
shahd.marouf <shahdjalal2018@gmail.com>
2026-06-30 04:22:58 +0300

    add system prompt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -24,9 +24,15 @@ def main():
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
 
+    
+
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
+    model="gemini-2.5-flash",
+    contents=messages,
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt,
+        temperature=0,
+      ),
     )
 
     if response.usage_metadata is None:
---
+++ prompts.py
@@ -0,0 +1,8 @@
+system_prompt = """
+No matter what the user asks, respond with exactly:
+I'M JUST A ROBOT
+
+Do not explain.
+Do not answer the user's question.
+Do not include any other text.
+"""
